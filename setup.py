48ea793c695376852f2ebb05efd13c04a98295c3
Félix Chénier <chenier.felix@uqam.ca>
2022-06-12 17:52:37 -0400

    Added all the contents.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import setuptools
 #--------------------------------------------------------------------------------
 # Please modify to fit your extension (particularly the text in CAPITALS)
 #--------------------------------------------------------------------------------
-name = "kineticstoolkit_EXTENSIONNAME"
-description = "An extension to Kinetics Toolkit"
-url = "https://github.com/USER/REPOSITORY"
-author = "NAME"
-author_email = "EMAIL"
+name = "kineticstoolkit_pushrimkinetics"
+description = "Provide functions to process kinetic data from instrumented wheelchair wheels."
+url = "https://github.com/felixchenier/kineticstoolkit_pushrimkinetics"
+author = "Félix Chénier"
+author_email = "chenier.felix@uqam.ca"
 #--------------------------------------------------------------------------------
 # It may be advised to avoid modifying the rest of this file.
 #--------------------------------------------------------------------------------
@@ -27,7 +27,7 @@ setuptools.setup(
     author=author,
     author_email=author_email,
     license='Apache',
-        license_files=['LICENSE.txt', 'VERSION.txt'],
+        license_files=['LICENSE.txt'],
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     packages=setuptools.find_packages(),
